1f4fcc4b5d5e70996c285b63af65106f09def7bb
Foysal Tech <smictfoysal@gmail.com>
2024-12-19 20:54:03 +0600

    blue vpn 7

---
+++ app/src/main/java/com/webihostapp/xprofreevpnapp/activities/GetStartedActivity.java
@@ -0,0 +1,74 @@
+package com.webihostapp.xprofreevpnapp.activities;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.webihostapp.xprofreevpnapp.AdSettings;
+import com.webihostapp.xprofreevpnapp.Preference;
+import com.webihostapp.xprofreevpnapp.customads.AdvertiseWebView;
+import com.webihostapp.xprofreevpnapp.MainApp;
+import com.webihostapp.xprofreevpnapp.R;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+
+
+    @SuppressLint("ObsoleteSdkInt")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getstarted);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        TextView txtbtn = findViewById(R.id.getstarted_btn);
+        TextView rateus = findViewById(R.id.rateus_btn);
+        AdvertiseWebView advertiseWebView = findViewById(R.id.advertiseWebView);
+
+        /*MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
+                for (String adapterClass : statusMap.keySet()) {
+                    AdapterStatus status = statusMap.get(adapterClass);
+                    Log.d("MyApp", String.format(
+                            "Adapter name: %s, Description: %s, Latency: %d",
+                            adapterClass, status.getDescription(), status.getLatency()));
+                }
+
+
+                RelativeLayout nativeAdLayout = findViewById(R.id.getstartednative);
+                AdsUtility.loadNativeAd(GetStartedActivity.this, nativeAdLayout, R.layout.native_ad_layout);
+            }
+        });*/
+
+        advertiseWebView.loadUrl(AdSettings.AD_URL_1);
+
+        Application application = getApplication();
+        if (!(application instanceof MainApp)) {
+            return;
+        }
+
+        txtbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GetStartedActivity.this, MainActivity.class);
+                startActivity(intent);
+                new Preference(GetStartedActivity.this).setBooleanpreference("ifsfr", true);
+            }
+        });
+        rateus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/webihostapp/xprofreevpnapp/customads/AdvertiseWebView.java
@@ -0,0 +1,73 @@
+package com.webihostapp.xprofreevpnapp.customads;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AdvertiseWebView extends WebView {
+
+    public AdvertiseWebView(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public AdvertiseWebView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public AdvertiseWebView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public AdvertiseWebView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        getSettings().setJavaScriptEnabled(true);
+        getSettings().setMediaPlaybackRequiresUserGesture(true);
+        getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("intent://")) {
+                    try {
+                        String modifiedUrl = url.replace("intent://", "http://");
+                        view.loadUrl(modifiedUrl);
+                        return true;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+        setWebChromeClient(new WebChromeClient());
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return true;
+    }
+
+    @Override
+    public void setOnTouchListener(OnTouchListener l) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+}
